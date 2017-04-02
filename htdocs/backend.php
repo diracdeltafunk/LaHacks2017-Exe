306a4de7587c91b32b762966ef9c86ebbbf93e35
Ben Spitz <benespitz@gmail.com>
2017-04-02 09:30:29 -0700

    Change tags

--- htdocs/backend.php
+++ htdocs/backend.php
@@ -1,4 +1,4 @@
-<?
+<?php
 
 $h = $_POST["diff"];
 $str = $_POST["q"].$_POST["pi"].$_POST["e"].$_POST["id"].$_POST["add"].$_POST["parts"].$_POST["neg"].$_POST["inv"].$_POST["exp"].$_POST["log"].$_POST["sine"].$_POST["cosine"].$_POST["asine"].$_POST["atan"];
