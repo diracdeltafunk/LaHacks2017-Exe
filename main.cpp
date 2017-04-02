2535d0c744d5c5003c7c8fc198c6366ee4a7d753
b2ogeyman <kostyamyagkov@gmail.com>
2017-04-02 00:12:52 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,24 @@
-#include "expression.h"
+#include <iostream>
+#include <set>
+#include "rules.hpp"
 
-int main(int argc, char** argv) {
+using namespace std;
+
+int main()
+{
+//	RationalNode l(Rational(1, 2)), r(Rational(1, 1));
+	set<Node*> c;
+	c.insert(new SinNode(new IdentityNode));
+	c.insert(new NegationNode(new SinNode(new IdentityNode)));
+	c.insert(new NegationNode(new SinNode(new IdentityNode)));
+	Expression expr(new AdditionNode(c));
+	set<Node*> cp;
+	cp.insert(new PatternMatchNode(0));
+	
+	cp.insert(new NegationNode(new PatternMatchNode(0)));
+	
+	Expression exprp(new AdditionNode(cp));
+	vector <vector <pair <int, Expression>>> v = PatternList(expr, exprp);
+	cout << v.size() << "\n";
     return 0;
 }
